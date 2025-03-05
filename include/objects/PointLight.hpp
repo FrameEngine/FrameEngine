2a1a6f55f1f6c0f2e149e851f11b254eb2eab2ee
Perchinka <alexsator.lukin@gmail.com>
2025-03-05 23:52:55 +0000

    Refactor objects/PointLight

--- include/objects/PointLight.hpp
+++ include/objects/PointLight.hpp
@@ -6,7 +6,11 @@
 
 /**
  * @class PointLight
- * @brief Represents a point light source as an Object in the ECS.
+ * @brief An ECS entity representing a point light source.
+ *
+ * PointLight extends Object, automatically inheriting a TransformComponent
+ * to manage its spatial position. However, it does not have a mesh associated
+ * with it.
  */
 class PointLight : public Object {
 private:
@@ -14,16 +18,49 @@ private:
   float intensity;
 
 public:
+  /**
+   * @brief Constructs a PointLight at a specified position.
+   *
+   * This constructor creates a PointLight without an associated mesh by passing
+   * a null pointer to the base Object constructor. The inherited
+   * TransformComponent is used to position the light within the world.
+   *
+   * @param registry The ECS registry in which this light is registered.
+   * @param pos The world-space position of the point light.
+   * @param col The color of the light (default is white: (1, 1, 1)).
+   * @param inten The intensity of the light (default is 1.0).
+   */
   PointLight(Registry &registry, const Vector3 &pos,
              const Vector3 &col = Vector3(1, 1, 1), float inten = 1.0f)
       : Object(registry, nullptr), color(col), intensity(inten) {
     transform->position = pos;
   }
 
+  /**
+   * @brief Retrieves the color of the point light.
+   *
+   * @return The current color of the light.
+   */
   Vector3 getColor() const { return color; }
+  /**
+   * @brief Sets the color of the point light.
+   *
+   * @param col The new color for the light.
+   */
   void setColor(const Vector3 &col) { color = col; }
 
+  /**
+   * @brief Retrieves the intensity of the point light.
+   *
+   * @return The current intensity of the light.
+   */
   float getIntensity() const { return intensity; }
+
+  /**
+   * @brief Sets the intensity of the point light.
+   *
+   * @param inten The new intensity value for the light.
+   */
   void setIntensity(float inten) { intensity = inten; }
 };
 
